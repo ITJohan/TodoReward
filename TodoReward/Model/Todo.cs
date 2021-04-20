50d9b6fb4696b26bc194df4d868172bf53a45a9e
Johan Lindkvist <johanlindkvist89@gmail.com>
2021-04-20 20:04:40 +0200

    Add empty todo and remove todo functionality

--- TodoReward/Model/Todo.cs
+++ TodoReward/Model/Todo.cs
@@ -7,6 +7,7 @@ namespace TodoReward.Model
 {
     public class Todo
     {
+        public int Id { get; set; }
         public string Description { get; set; }
         public int TimeLeft { get; set; }
         public int RepeatInterval { get; set; }
