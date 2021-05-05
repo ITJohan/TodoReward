97004e0715f945fe227b110eae3e81c0a5a7a8d4
Johan Lindkvist <johanlindkvist89@gmail.com>
2021-05-05 18:05:54 +0200

    Add todo repository skeleton

---
+++ TodoReward/Repository/ITodoRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoReward.Model;
+
+namespace TodoReward.Repository
+{
+    interface ITodoRepository
+    {
+        bool AddTodo(Todo todo);
+        List<Todo> GetTodos();
+        Todo GetTodo(int id);
+        bool UpdateTodo(Todo todo);
+        bool DeleteTodo(int id);
+    }
+}
---
+++ TodoReward/Repository/TodoRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoReward.Model;
+
+namespace TodoReward.Repository
+{
+    public class TodoRepository : ITodoRepository
+    {
+        public bool AddTodo(Todo todo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteTodo(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Todo GetTodo(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Todo> GetTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateTodo(Todo todo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
