95e3eec4adfc7e4ae75a45a983174c08ec9179e6
Johan Lindkvist <johanlindkvist89@gmail.com>
2021-05-10 18:23:02 +0200

    Implement reward repository functionality in rewards

--- TodoReward/Repository/RewardRepository.cs
+++ TodoReward/Repository/RewardRepository.cs
@@ -10,7 +10,33 @@ namespace TodoReward.Repository
 
         public RewardRepository()
         {
-            _rewards = new List<Reward>();
+            _rewards = new List<Reward>
+            {
+                new Reward
+                {
+                    Id = 1,
+                    Description = "1h gaming",
+                    Points = -10
+                },
+                new Reward
+                {
+                    Id = 2,
+                    Description = "Æt nått gott",
+                    Points = -100
+                },
+                new Reward
+                {
+                    Id = 3,
+                    Description = "Fylla",
+                    Points = -10
+                },
+                new Reward
+                {
+                    Id = 4,
+                    Description = "Drønare",
+                    Points = -1000
+                }
+            };
         }
 
         public bool AddReward(Reward reward)
