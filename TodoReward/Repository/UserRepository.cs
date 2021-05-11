8e1addea80bdf4acaaad5abdd788e69de457ee97
Johan Lindkvist <johanlindkvist89@gmail.com>
2021-05-11 19:59:07 +0200

    Add user repository skeleton

---
+++ TodoReward/Repository/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoReward.Model;
+
+namespace TodoReward.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        public void AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUser(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveUser(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
