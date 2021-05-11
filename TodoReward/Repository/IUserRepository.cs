dbf764d6d75fef39fe76443a7c9c43e68758b278
Johan Lindkvist <johanlindkvist89@gmail.com>
2021-05-11 19:58:02 +0200

    Add user repository interface

---
+++ TodoReward/Repository/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoReward.Model;
+
+namespace TodoReward.Repository
+{
+    public interface IUserRepository
+    {
+        void AddUser(User user);
+        void RemoveUser(Guid id);
+        void UpdateUser(User user);
+        User GetUser(Guid id);
+        List<User> GetUsers();
+    }
+}
