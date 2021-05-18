1d711bbb361a5e3961ba393f869ac8d02fbe4ddb
Johan Lindkvist <johanlindkvist89@gmail.com>
2021-05-18 19:29:02 +0200

    Move model and repository to data folder and create user state container

---
+++ TodoReward/Data/UserStateContainer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoReward.Model;
+
+namespace TodoReward.Data
+{
+    public class UserStateContainer
+    {
+        private User _user;
+        public User User
+        {
+            get => _user;
+            set
+            {
+                _user = value;
+                NotifyStateChanged();
+            }
+        }
+
+        public event Action OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
